44d186f5790ab1db020254b7136dd07a34f68ff3
Yaroslav Nizhankovskyi <yaroslav.nizhankovskyi@gmail.com>
2022-07-07 13:17:56 +0300

    Fixed modules

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   clientId: '8db57514-3ac4-431a-808e-e39a8e4041d5', 
   authority: '940a914e-05a6-4807-8e22-374cf7a71990', 
-  redirectUri: 'https://localhost:4200',
+  redirectUri: 'https://calm-rock-0b0645110.1.azurestaticapps.net',
   graphEndpoint: 'https://graph.microsoft.com/v1.0/me',
   cloudInstance: 'https://login.microsoftonline.com',
   cacheLocation: 'localStorage',
